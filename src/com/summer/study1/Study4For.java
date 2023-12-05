b580cc9b6dce458e36991030de88ec1f8d6aeeae
dirokim <dirokim7@gmail.com>
2023-12-05 14:49:41 +0900

    as

--- src/com/summer/study1/Study4For.java
+++ src/com/summer/study1/Study4For.java
@@ -1,7 +1,14 @@
 package com.summer.study1;
 
+import java.util.Scanner;
+
 public class Study4For {
 	public static void main(String[]agrs) {
-		System.out.println("Master");
+		//서든 FPS 
+		//총알 : 30발 탄창 3
+		//1.단발모드 1발
+		//2.점사모드 10발
+		
+
 	}
 }
