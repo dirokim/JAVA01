b4bb17e89aeaded4eb7abc9ac63b5a09ec79da92
dirokim <dirokim7@gmail.com>
2023-12-05 15:22:37 +0900

    ad

--- src/com/summer/study1/Study3.java
+++ src/com/summer/study1/Study3.java
@@ -10,19 +10,23 @@ public class Study3 {
 		//0-59초
 		//1분 5초 입력
 		//분과 초를 입력받아
-
+		System.out.println("초 입력하세요");
+		int sec = sc.nextInt();
+		System.out.println("분 입력하세요");
+		int min = sc.nextInt();
 			boolean flag = false;
 		for(int i=1;i<60;i++) {
-			System.out.println("분 입력하세요");
-			int num = sc.nextInt();
-		
 			for(int j=1;j<60;j++) {
-				System.out.println("초 입력하세요");
-				int num1 = sc.nextInt();
-
+				if(min==i&&sec==j){
+					break;
+				}
 				
 				}
-				}
+//			if(flag) {
+//				break;
+//			}
+			
+		}
 			
 		
 				
