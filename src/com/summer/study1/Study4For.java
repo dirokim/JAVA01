524a6e0f8e084b60248ed6b70cfba9f7f050e753
dirokim <dirokim7@gmail.com>
2023-12-05 15:06:11 +0900

    ad

--- src/com/summer/study1/Study4For.java
+++ src/com/summer/study1/Study4For.java
@@ -8,7 +8,27 @@ public class Study4For {
 		//총알 : 30발 탄창 3
 		//1.단발모드 1발
 		//2.점사모드 10발
+		Scanner sc = new Scanner(System.in);
+		System.out.println("모드를 정하세요1or2");
+		int num = sc.nextInt();
 		
+		if(num ==1){
+			for(int i =1;i<=3;i++) {
+				for(int j = 1;j<=30;j++) {
+					System.out.println(j);
+					
+			}
+			}
+	
+		}else if(num ==2) {
+			for(int i=1;i<=3;i++) {
+				for(int j = 1; j<31;j++) {
+					j+=10;
+					j-=1;
+					System.out.println(j);
+				}
+			}
+		}
 
 	}
 }
