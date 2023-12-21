f99b2d33ee7721ffa712618309cfa8fa89856fc3
dirokim <dirokim7@gmail.com>
2023-12-21 12:24:22 +0900

    ad

--- src/com/summer/study7/Client.java
+++ src/com/summer/study7/Client.java
@@ -1,15 +1,57 @@
 package com.summer.study7;
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
 	public void send() throws Exception {
-		Socket socket = new Socket("192.168.7.191", 8282);
+		Scanner sc = new Scanner(System.in);
+		Socket socket = new Socket("localhost", 8283);
 		System.out.println("서버와 접속이 되었습니다.");
+		
+		while(true) {
+		System.out.println("서버로 보낼 메세지 보냄");
+		String m = sc.next();
+		OutputStream os = socket.getOutputStream(); //0과1 처리
+		OutputStreamWriter ow = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(ow);
+		
+		bw.write(m+"\n\r");
+		bw.flush();
+		
+		System.out.println("서버로부터 메세지받음");
+		
+		InputStream is = socket.getInputStream();
+		InputStreamReader ir = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(ir);
+		
+		System.out.println(br.readLine());
+		
+		if(m.equals("exit")) {
+			break;
+		}
+		
+		
+		
+		}
+		
+		
+		
+
+		
+	
+		
 		
 	}
 	
--- src/com/summer/study7/Server.java
+++ src/com/summer/study7/Server.java
@@ -1,20 +1,58 @@
 package com.summer.study7;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
 	
 	public void s() throws Exception {
 		System.out.println("클라이언트가 접속하기를 기다리는중 ");
-		ServerSocket ss = new ServerSocket(8282);
+		ServerSocket ss = new ServerSocket(8283);
+		
+		
 		Socket sc = ss.accept();
 		System.out.println("Client 연결 성공 ");
 		
+		while(true) {
+			System.out.println("클라이언트로부터 메세지받음");
+		InputStream is = sc.getInputStream();
+		InputStreamReader ir = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(ir);
+		
+		System.out.println(br.readLine());
+
+		//받아서 반대로 클라이언트한테 다시 메세지 전송
+		
+		System.out.println("클라이언트한테 메세지보냄");
+		OutputStream os =  sc.getOutputStream();
+		OutputStreamWriter ow = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(ow);
+		Scanner sc1 = new Scanner(System.in);
+		String abc = sc1.next();
+		bw.write(abc+"\n\r");
+		bw.flush();
 		
 		
-		System.out.println("서버 프로그램 종료");
+		
+		if(abc.equals("exit")) {
+			break;
+		}
+		}
+		
+		
+		
+		
+	
+	
+
 
 	}
 	
