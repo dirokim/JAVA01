44b12256a34768fd6472f6b27255c2e47b2bd401
dirokim <dirokim7@gamail.com>
2023-12-23 12:03:09 +0900

    ad

---
+++ src/com/summer/study7/ex1/server2/Test.java
@@ -0,0 +1,15 @@
+package com.summer.study7.ex1.server2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Test2 test = new Test2();
+		test.start();
+		
+		
+		
+	}
+
+}
---
+++ src/com/summer/study7/ex1/server2/Test2.java
@@ -0,0 +1,9 @@
+package com.summer.study7.ex1.server2;
+
+public class Test2 {
+
+	public void start() {
+		
+		System.out.println("1"+1);
+	}
+}
